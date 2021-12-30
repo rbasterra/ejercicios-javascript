9db61bd3e346ca951d4ac4a266effb713ae3689e
Rafael Basterra Careaga <rbasterra@msn.com>
2021-12-30 19:18:37 +0100

    Ejercicio#5

---
+++ Ejercicio5.js
@@ -0,0 +1,5 @@
+// Dado un array de números, muestra por consola un array con dichos números ordenados en forma descendente.
+
+let num = [20,3,4,6,2, 16, 9, 1];
+
+console.log(num.sort((a,b) =>  a - b));
