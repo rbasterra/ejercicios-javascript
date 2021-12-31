4cc8dd6d39f37d646e95925c3c101928a2b0d5f3
Rafael Basterra Careaga <rbasterra@msn.com>
2021-12-31 10:30:42 +0100

    Ejercicio#6

---
+++ Ejercicio6.js
@@ -0,0 +1,7 @@
+// Dado un array de strings, muestra por consola un array con sus valores ordenadosalfabéticamente. 
+// Por ejemplo:
+// ['b', 'c', 'd', 'a'] => output: ['a', 'b', 'c', 'd']
+
+let strs = ['b', 'c', 'd', 'a', 'z', 'l', 'x', 'e'];
+
+console.log(strs.sort());
